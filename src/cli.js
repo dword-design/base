9c215d23f823cd6a01141b9bec80c719ff7755d9
Sebastian Landwehr <info@dword-design.de>
2019-05-25 18:13:31 +0200

    lint command

--- src/cli.js
+++ src/cli.js
@@ -5,25 +5,30 @@ const path = require('path')
 const readPkgUp = require('read-pkg-up')
 const yargs = require('yargs')
 
+const lint = ({ packagePath }) => spawn(
+  'eslint',
+  ['.', '--config', path.resolve(packagePath, 'src/eslintrc.js'), '--ignore-path', path.resolve(packagePath, 'src/gitignore')],
+  { stdio: 'inherit' }
+)
+
+const build = context => {
+  const { packagePath } = context
+  return lint(context)
+    .then(() => spawn(
+      'babel',
+      ['src', '--out-dir', 'dist', '--config-file', path.resolve(packagePath, 'src/babel.config.js')],
+      { stdio: 'inherit' }
+    ))
+}
+
 Promise.all([readPkgUp({ cwd: __dirname }), readPkgUp()])
   .then(([{ path: packageFilePath }, { package: { type = 'node' } }]) =>
     ({ packagePath: path.dirname(packageFilePath), type })
   )
-  .then(({ packagePath }) => yargs
-    .command({
-      command: 'build',
-      handler: () => Promise.resolve()
-        .then(() => spawn(
-          'eslint',
-          ['.', '--config', path.resolve(packagePath, 'src/eslintrc.js'), '--ignore-path', path.resolve(packagePath, 'src/gitignore')],
-          { stdio: 'inherit' }
-        ))
-        .then(() => spawn(
-          'babel',
-          ['src', '--out-dir', 'dist', '--config-file', path.resolve(packagePath, 'src/babel.config.js')],
-          { stdio: 'inherit' }
-        ))
-        .catch(() => {}),
-    })
-    .argv
-  )
+  .then(context => {
+    const { packagePath } = context
+    yargs
+      .command({ command: 'lint', handler: () => lint(context).catch(() => {}) })
+      .command({ command: 'build', handler: () => build(context).catch(() => {}) })
+      .argv
+  })
