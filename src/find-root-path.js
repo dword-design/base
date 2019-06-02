52ef4d9bb4a7531a11ec55cb3c356fb699deec19
Sebastian Landwehr <info@dword-design.de>
2019-06-02 09:17:27 +0200

    fix find root path

--- src/find-root-path.js
+++ src/find-root-path.js
@@ -2,8 +2,10 @@ const readPkgUp = require('read-pkg-up')
 const path = require('path')
 
 module.exports = () => readPkgUp()
-  .then(({ path: packagePath }) => readPkgUp({ cwd: path.resolve(packagePath, '..') })
-    .then(({ path: parentPath, package: { workspaces, private } = {} } = {}) =>
-      path.dirname(workspaces !== undefined && private ? parentPath : packagePath)
-    )
+  .then(({ path: packagePath } = {}) => packagePath !== undefined
+    ? readPkgUp({ cwd: path.resolve(packagePath, '..') })
+      .then(({ path: parentPath, package: { workspaces, private } = {} } = {}) =>
+        path.dirname(workspaces !== undefined && private ? parentPath : packagePath)
+      )
+    : Promise.resolve()
   )
