f8314552c2dfa781eccea5333037181b233f2372
Sebastian Landwehr <info@dword-design.de>
2019-07-18 01:07:21 +0200

    add formatters

--- src/commands/depgraph.js
+++ src/commands/depgraph.js
@@ -3,12 +3,65 @@ const path = require('path')
 const { spawn } = require('child-process-promise')
 const findBasePath = require('../find-base-path')
 const findConfig = require('../find-config')
-const { keys } = require('lodash')
+const { keys, chain } = require('lodash')
+const depcruise = require('dependency-cruiser').cruise
+
+const formatters = {
+  dot: {
+    depcruiseOptions: {
+      outputType: 'dot',
+    },
+    handler: (dotStructure, basePath) => Promise.resolve()
+      .then(() => spawn('dot', ['-T', 'svg'], { capture: ['stdout'] })
+        .progress(({ stdin }) => {
+          stdin.write(dotStructure)
+          stdin.end()
+        })
+      )
+      .then(({ stdout: svgCode }) =>
+        spawn(
+          path.resolve(basePath, 'node_modules/.bin/open-cli'),
+          ['--extension', 'html'],
+        )
+          .progress(({ stdin }) => {
+            stdin.write(svgCode)
+            stdin.end()
+          })
+      )
+  },
+  json: {
+    handler: modules => console.log(JSON.stringify(modules)),
+  },
+  puml: {
+    handler: modules => console.log(
+      [
+        '@startuml',
+        ...modules.map(({ source }) => `Class "${source}"`),
+        ...chain(modules)
+          .flatMap(({ source, dependencies }) => dependencies.map(({ resolved: target }) => ({ source, target })))
+          .map(({ source, target }) => `"${source}" --> "${target}"`),
+        '@enduml',
+      ]
+        .join('\r\n')
+    ),
+  }
+}
+
+const getFormatter = formatterName => {
+  const formatter = formatters[formatterName]
+  if (formatter === undefined) {
+    throw new Error(`A formatter with the name '${formatterName}' does not exist.`)
+  }
+  return formatter
+}
 
 module.exports = {
   name: 'depgraph',
   desc: 'Outputs a dependency graph for the current workspace',
-  handler: () => Promise.all([findBasePath(), findConfig()])
+  options: [
+    { name: '-t, --format <formatterName>', desc: 'The output format', defaultValue: 'dot' },
+  ],
+  handler: ({ format: formatterName }) => Promise.all([findBasePath(), findConfig()])
     .then(([basePath, { depgraphIgnores }]) => {
 
       const ignores = [
@@ -17,26 +70,18 @@ module.exports = {
         ...depgraphIgnores,
       ]
 
-      return spawn(
-        path.resolve(basePath, 'node_modules/.bin/depcruise'),
-        ['-x', `(${ignores.join('|')})`, '-T', 'dot', '.'],
-        { capture: ['stdout'] },
+      const { handler, depcruiseOptions } = getFormatter(formatterName)
+
+      return handler(
+        depcruise(
+          ['.'],
+          {
+            exclude: `(${ignores.join('|')})`,
+            ...depcruiseOptions,
+          }
+        ).modules,
+        basePath
       )
-        .then(({ stdout: dotStructure }) => spawn('dot', ['-T', 'svg'], { capture: ['stdout'] })
-          .progress(({ stdin }) => {
-            stdin.write(dotStructure)
-            stdin.end()
-          })
-        )
-        .then(({ stdout: svgCode }) => spawn(
-          path.resolve(basePath, 'node_modules/.bin/open-cli'),
-          ['--extension', 'html'],
-        )
-          .progress(({ stdin }) => {
-            stdin.write(svgCode)
-            stdin.end()
-          })
-        )
     }),
   isEnabled: nodeEnv === 'development',
 }
