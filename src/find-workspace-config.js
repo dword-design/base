f5abb3594287129748d59e5425c468f24c067c56
Sebastian Landwehr <info@dword-design.de>
2019-07-29 03:02:43 +0200

    fix workspace config for missing package.json

--- src/find-workspace-config.js
+++ src/find-workspace-config.js
@@ -3,6 +3,6 @@ const getType = require('./get-type')
 const { omit } = require('lodash')
 
 module.exports = () => {
-  const config = { typeName: 'lib', ...readPkgUp.sync().package || {} }
+  const config = { typeName: 'lib', ...(readPkgUp.sync() || {}).package || {} }
   return { ...omit(config, 'typeName'), type: getType(config.typeName) }
 }
