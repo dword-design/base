24aa530fdc417ae6cf4c05d604cbcece5f3e5501
Sebastian Landwehr <info@dword-design.de>
2019-09-05 00:28:46 +0200

    fix test glob

--- src/test.js
+++ src/test.js
@@ -6,33 +6,39 @@ const resolveBin = require('resolve-bin')
 const glob = require('glob-promise')
 
 Promise.resolve()
-  .then(() => fork(
-    resolveBin.sync('jest'),
-    [
-      '\.test\.js$',
-      '--testPathIgnorePatterns', '\.docker\.test\.js$',
-      '--transform', `{"\.js":"${resolve(__dirname, 'jest-transformer.js')}"}`,
-    ],
-  ))
-  .then(() => glob('**/*.docker.test.js'))
-  .then(filenames => filenames.length > 0
-    ? Promise.resolve()
-      .then(() => console.log('Starting docker container …'))
-      .then(() => spawn(
-        'docker',
+  .then(() => glob('**/*.test.js', { ignore: ['**/*.docker.test.js', '**/node_modules/**'] })
+    .then(filenames => filenames.length > 0
+      ? fork(
+        resolveBin.sync('jest'),
         [
-          'run',
-          '-e', 'NODE_ENV=production',
-          '-u', 'node',
-          '-v', `${process.cwd()}:/var/www`,
-          '-w', '/var/www',
-          'node:alpine',
-          relative(process.cwd(), resolveBin.sync('jest')),
-          '\.docker\.test\.js$',
-          '--transform', `{"\.js":"<rootDir>/${relative(process.cwd(), resolve(__dirname, 'jest-transformer.js'))}"}`,
+          '\.test\.js$',
+          '--testPathIgnorePatterns', '\.docker\.test\.js$',
+          '--transform', `{"\.js":"${resolve(__dirname, 'jest-transformer.js')}"}`,
         ],
-        { stdio: 'inherit' },
-      ))
-    : undefined
+      )
+      : undefined
+    )
+  )
+  .then(() => glob('**/*.docker.test.js', { ignore: '**/node_modules/**' })
+    .then(filenames => filenames.length > 0
+      ? Promise.resolve()
+        .then(() => console.log('Starting docker container …'))
+        .then(() => spawn(
+          'docker',
+          [
+            'run',
+            '-e', 'NODE_ENV=production',
+            '-u', 'node',
+            '-v', `${process.cwd()}:/var/www`,
+            '-w', '/var/www',
+            'node:alpine',
+            relative(process.cwd(), resolveBin.sync('jest')),
+            '\.docker\.test\.js$',
+            '--transform', `{"\.js":"<rootDir>/${relative(process.cwd(), resolve(__dirname, 'jest-transformer.js'))}"}`,
+          ],
+          { stdio: 'inherit' },
+        ))
+      : undefined
+    )
   )
   .catch(({ name, code }) => name === 'ChildProcessError' ? process.exit(code) : undefined)
