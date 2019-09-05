bc3a7b77129cef066fa04ec12c4d0ff337045c6d
Sebastian Landwehr <info@dword-design.de>
2019-09-05 18:08:45 +0200

    run tests pre-commit

--- src/commands/pre-commit.js
+++ src/commands/pre-commit.js
@@ -1,12 +1,14 @@
 const nodeEnv = require('@dword-design/node-env')
 const lintStaged = require('./lint-staged')
 const depcheck = require('./depcheck')
+const test = require('./test')
 
 module.exports = {
   name: 'pre-commit',
   description: 'Runs commands before committing',
   handler: () => Promise.resolve()
     .then(lintStaged.handler)
-    .then(depcheck.handler),
+    .then(depcheck.handler)
+    .then(test.handler),
   isEnabled: nodeEnv === 'development',
 }
