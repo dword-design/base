653b364d0a383ad88f433c8aab4762acd2514ba9
Sebastian Landwehr <info@dword-design.de>
2019-09-06 20:20:50 +0200

    add jest fp

--- src/get-aliases.js
+++ src/get-aliases.js
@@ -1,8 +1,10 @@
 const getVariablesPath = require('./get-variables-path')
 const getType = require('./get-type')
+const { resolve } = require('path')
 
 module.exports = () => ({
   '@variables': getVariablesPath(),
   '@functions': require.resolve('@dword-design/functions'),
+  '@test': require.resolve('@dword-design/jest-expect-fp'),
   ...getType().aliases,
 })
