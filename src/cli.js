f1d42322eed48aad3340ba6f990bc46a7c9f5c3f
Sebastian Landwehr <info@dword-design.de>
2019-10-05 16:27:32 +0200

    stand

--- src/cli.js
+++ src/cli.js
@@ -1,38 +1,5 @@
 #!/usr/bin/env node
 
-const { fork } = require('child-process-promise')
-const { chain, some, map } = require('lodash')
-const getType = require('./get-type')
-const yesSir = require('@dword-design/yes-sir')
+const api = require('.')
 
-const commands = require('./commands')
-const type = getType()
-
-yesSir({
-  commands: map(
-    [
-      ...chain(commands)
-        .map(command => ({ isEnabled: true, ...command }))
-        .filter({ isEnabled: true }),
-      ...chain(type.commands)
-        .filter(({ name }) => !some(commands, { name }))
-        .map(command => ({
-          ...command,
-          handler: () => fork(require.resolve('./run-workspace-command'), [command.name]),
-        }))
-        .value(),
-    ],
-    command => ({
-      ...command,
-      handler: (...args) => command.handler(...args)
-        .catch(error => {
-          if (error.name === 'ChildProcessError') {
-            process.exit(error.code)
-          } else {
-            throw error
-          }
-        }),
-    })
-  ),
-  defaultCommandName: 'install',
-})
+api[process.argv[2]]()
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 module.exports = {
-  getAliases: require('./get-aliases'),
-  getBaseConfig: require('./get-base-config'),
-  gitignore: require('./gitignore'),
+
 }
