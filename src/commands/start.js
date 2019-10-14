0cba313db5c86efd97bb0ca9815f7fb477e692ae
Sebastian Landwehr <info@dword-design.de>
2019-10-14 20:33:27 +0200

    allow not to pass log for start

--- src/commands/start.js
+++ src/commands/start.js
@@ -3,7 +3,7 @@ const getTarget = require('../get-target')
 module.exports = {
   name: 'start',
   description: 'Starts the package',
-  handler: async ({ log }) => {
+  handler: async ({ log } = {}) => {
     const { start } = getTarget()
     await start({ log })
   },
