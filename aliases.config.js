74c2d990be9c136a493fa655896f5622de98d944
dword <sebastianlandw@gmail.com>
2019-10-31 17:33:42 +0000

    add node-env and eslint alias

--- aliases.config.js
+++ aliases.config.js
@@ -2,4 +2,6 @@ module.exports = {
   '@functions': '@dword-design/functions',
   'fs': 'fs-extra',
   'child_process': 'child-process-promise',
+  'node-env': 'better-node-env',
+  'eslint': 'eslint',
 }
