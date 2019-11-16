4bac8b64ce99031c080002fc17e263555a9b40f7
Sebastian Landwehr <info@dword-design.de>
2019-11-16 14:33:26 +0100

    fix test command and write first test for valid test

--- src/get-commands.js
+++ src/get-commands.js
@@ -56,7 +56,17 @@ export default ({ prepare: configPrepare, start: configStart } = {}) => {
         if (nodeEnv === 'development') {
           await spawn(resolveBin.sync('install-self'), [])
         }
-        await spawn(resolveBin.sync('nyc'), ['--reporter', 'lcov', '--reporter', 'text', resolveBin.sync('mocha-per-file'), '--require', require.resolve('./pretest')], { stdio: 'inherit' })
+        await spawn(
+          resolveBin.sync('nyc'),
+          [
+            '--reporter', 'lcov',
+            '--reporter', 'text',
+            '--cwd', process.cwd(),
+            resolveBin.sync('mocha-per-file'),
+            '--require', require.resolve('./pretest'),
+          ],
+          { stdio: 'inherit' }
+        )
       },
     },
     register: {
---
+++ test/test/valid.test.js
@@ -0,0 +1,36 @@
+import outputFiles from 'output-files'
+import { spawn } from 'child_process'
+import resolveBin from 'resolve-bin'
+import withLocalTmpDir from 'with-local-tmp-dir'
+import { endent } from '@functions'
+import expect from 'expect'
+
+export const it = () => withLocalTmpDir(__dirname, async () => {
+  await outputFiles({
+    'src/index.js': 'export default 1',
+    'package.json': JSON.stringify({
+      name: 'foo',
+      main: 'dist/index.js',
+      files: [
+        'dist',
+      ],
+      devDependencies: {
+        expect: '^0.1.0',
+      },
+    }),
+    'test/foo.test.js': endent`
+      import foo from 'foo'
+      import expect from 'expect'
+
+      export default () => expect(foo).toEqual(1)
+    `,
+  })
+  const { stdout } = await spawn(
+    resolveBin.sync('@dword-design/base', { executable: 'base' }),
+    ['test'],
+    { capture: ['stdout'] }
+  )
+  expect(stdout).toMatch(/^Copying config files …\nSuccessfully compiled 1 file with Babel.\nNo depcheck issue\n\n\n  ✓ foo\n\n  1 passing.*?\n\n----------|----------|----------|----------|----------|-------------------|\nFile      |  % Stmts | % Branch |  % Funcs |  % Lines | Uncovered Line #s |\n----------|----------|----------|----------|----------|-------------------|\nAll files |        0 |        0 |        0 |        0 |                   |\n----------|----------|----------|----------|----------|-------------------|\n$/)
+})
+export const only = true
+export const timeout = 20000
