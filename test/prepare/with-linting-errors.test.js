beca42177368ed8a492c992df9ef9c518e6ad17a
dword <sebastianlandw@gmail.com>
2019-11-17 22:36:06 +0000

    test that dist is missing after errored build

--- test/prepare/with-linting-errors.test.js
+++ test/prepare/with-linting-errors.test.js
@@ -1,12 +1,13 @@
 import withLocalTmpDir from 'with-local-tmp-dir'
 import resolveBin from 'resolve-bin'
 import { spawn } from 'child_process'
-import { outputFile } from 'fs'
+import { outputFile, exists } from 'fs'
 import expect from 'expect'
 
 export const it = () => withLocalTmpDir(__dirname, async () => {
   await outputFile('src/index.js', 'console.log(\'hi\');')
   await expect(spawn(resolveBin.sync('@dword-design/base', { executable: 'base' }), ['prepare'])).rejects.toThrow()
+  expect(await exists('dist')).toBeFalsy()
 })
 
 export const timeout = 8000
