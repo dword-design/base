faefa2d9b445f0f141577a9ef58a2bb08f7d6a97
dword <sebastianlandw@gmail.com>
2019-12-04 23:08:37 +0000

    remove babel config from pretest

--- src/pretest.js
+++ src/pretest.js
@@ -1,7 +1,3 @@
 import babelRegister from '@babel/register'
-import expect from 'expect'
-import config from '@dword-design/babel-config'
 
-babelRegister(config)
-
-global.expect = expect
+babelRegister()
