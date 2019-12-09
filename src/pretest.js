4874a243a6cf73e43d7878a93379bd21c8d62629
dword <sebastianlandw@gmail.com>
2019-12-09 23:48:46 +0000

    remove spawn-wrap

--- src/pretest.js
+++ src/pretest.js
@@ -1,11 +1,8 @@
 import babelRegister from '@babel/register'
 import babelConfig from '@dword-design/babel-config'
-import spawnWrap from 'spawn-wrap'
 import moduleAlias from 'module-alias'
 import { getForTests as getAliasesForTests } from '@dword-design/aliases'
 
 babelRegister({ ...babelConfig, ignore: [/node_modules/] })
 
 moduleAlias.addAliases(getAliasesForTests())
-
-spawnWrap([require.resolve('./spawn-wrap')])
--- src/spawn-wrap.js
+++
@@ -1,7 +0,0 @@
-import babelRegister from '@babel/register'
-import babelConfig from '@dword-design/babel-config'
-import spawnWrap from 'spawn-wrap'
-
-babelRegister({ ...babelConfig, ignore: [/node_modules/] })
-
-spawnWrap.runMain()
