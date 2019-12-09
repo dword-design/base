7c426e733d3304381e7c32ecffa505b59adfd44b
dword <sebastianlandw@gmail.com>
2019-12-09 23:45:22 +0000

    refactoring

--- src/pretest.js
+++ src/pretest.js
@@ -2,11 +2,10 @@ import babelRegister from '@babel/register'
 import babelConfig from '@dword-design/babel-config'
 import spawnWrap from 'spawn-wrap'
 import moduleAlias from 'module-alias'
-import { forIn } from '@functions'
 import { getForTests as getAliasesForTests } from '@dword-design/aliases'
 
 babelRegister({ ...babelConfig, ignore: [/node_modules/] })
 
-getAliasesForTests() |> forIn((path, name) => moduleAlias.addAlias(name, path))
+moduleAlias.addAliases(getAliasesForTests())
 
-spawnWrap([require.resolve('./spawn-wrap.js')])
+spawnWrap([require.resolve('./spawn-wrap')])
