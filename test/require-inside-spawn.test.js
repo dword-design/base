aeb1f6f4e3dff406455f615fff387ab8e426da35
Sebastian Landwehr <info@dword-design.de>
2019-12-11 22:24:50 +0100

    fix require spawn test

--- test/require-inside-spawn.test.js
+++ test/require-inside-spawn.test.js
@@ -10,23 +10,20 @@ import P from 'path'
 export const it = () => withLocalTmpDir(__dirname, async () => {
   await outputFiles({
     ...minimalProjectConfig,
-    'node_modules/.bin/foo-cli': endent`
-      #!/usr/bin/env node
-
-      const importFrom = require('import-from')
-
-      console.log(importFrom(process.cwd(), 'foo'))
-    `,
     test: {
+      'cli.js': endent`
+        #!/usr/bin/env node
+
+        console.log('foo')
+      `,
       'works.test.js': endent`
         import { spawn } from 'child_process'
 
-        export default () => spawn('foo-cli', [], { stdio: 'inherit' })
+        export default () => spawn(require.resolve('./cli'), [], { stdio: 'inherit' })
       `,
     },
   })
-  await chmod(P.join('node_modules', '.bin', 'foo-cli'), '755')
-  await spawn('npm', ['test'], { stdio: 'inherit' })
+  await chmod(P.join('test', 'cli.js'), '755')
   const { stdout } = await spawn('base', ['test'], { capture: ['stdout'] })
   expect(stdout).toMatch(new RegExp(endent`
     ^package.json valid
