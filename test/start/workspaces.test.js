568090e1f4e1b600744be3d8bc08315f24ce9fbf
Sebastian Landwehr <info@dword-design.de>
2019-12-16 17:57:26 +0100

    fix test to start workspaces

--- test/start/workspaces.test.js
+++ test/start/workspaces.test.js
@@ -7,6 +7,7 @@ import stealthyRequire from 'stealthy-require'
 import P from 'path'
 import expect from 'expect'
 import { outputFile } from 'fs-extra'
+import waitForChange from 'wait-for-change'
 
 export const it = () => withLocalTmpDir(__dirname, async () => {
   await outputFiles({
@@ -19,33 +20,27 @@ export const it = () => withLocalTmpDir(__dirname, async () => {
     'packages/a': minimalWorkspaceConfig,
     'packages/b': minimalWorkspaceConfig,
   })
-  const childProcess = spawn('base', ['start'])
+  const childProcess = spawn('base', ['start'], { stdio: 'ignore' })
     .catch(error => {
       if (error.code !== null) {
         throw error
       }
     })
     .childProcess
-  let successes = 0
-  const finishedListener = cb => data => {
-    console.log(data.toString())
-    if (data.toString() === 'Successfully compiled 1 file with Babel.\n') {
-      ++successes
-      if (successes >= 2) {
-        childProcess.stdout.removeListener('data', finishedListener)
-        cb()
-      }
-    }
-  }
-  await new Promise(resolve => childProcess.stdout.on('data', finishedListener(resolve)))
+  await Promise.all([
+    waitForChange(P.join('packages', 'a', 'dist', 'index.js')),
+    waitForChange(P.join('packages', 'b', 'dist', 'index.js')),
+  ])
   expect(require(P.resolve('packages', 'a', 'dist'))).toEqual(1)
   expect(require(P.resolve('packages', 'b', 'dist'))).toEqual(1)
   await Promise.all([
     outputFile(P.resolve('packages', 'a', 'src', 'index.js'), 'export default 2'),
     outputFile(P.resolve('packages', 'b', 'src', 'index.js'), 'export default 2'),
   ])
-  successes = 0
-  await new Promise(resolve => childProcess.stdout.on('data', finishedListener(resolve)))
+  await Promise.all([
+    waitForChange(P.join('packages', 'a', 'dist', 'index.js')),
+    waitForChange(P.join('packages', 'b', 'dist', 'index.js')),
+  ])
   expect(stealthyRequire(require.cache, () => require(P.resolve('packages', 'a', 'dist')))).toEqual(2)
   expect(stealthyRequire(require.cache, () => require(P.resolve('packages', 'b', 'dist')))).toEqual(2)
   childProcess.kill()
