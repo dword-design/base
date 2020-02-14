9868836dddf464455c0b0faaa4afafff6a3f64ab
Sebastian Landwehr <info@dword-design.de>
2020-02-15 00:43:19 +0100

    fix depgraph test

--- src/depgraph.spec.js
+++ src/depgraph.spec.js
@@ -1,17 +1,11 @@
 import withLocalTmpDir from 'with-local-tmp-dir'
 import depgraph from './depgraph'
-import kill from 'tree-kill'
 import portReady from 'port-ready'
+import kill from 'tree-kill'
 
 export default {
   valid: () => withLocalTmpDir(async () => {
     const childProcess = depgraph({ log: false })
-      .catch(error => {
-        if (error.code !== null) {
-          throw error
-        }
-      })
-      .childProcess
     await portReady(3000)
     kill(childProcess.pid)
   }),
