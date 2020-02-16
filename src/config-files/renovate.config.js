3d5a0f2845552b1985932fe15bc807360d543d16
Sebastian Landwehr <info@dword-design.de>
2020-02-16 01:43:47 +0100

    enable semantic commits for renovate

--- src/config-files/renovate.config.js
+++ src/config-files/renovate.config.js
@@ -2,6 +2,10 @@ import { endent } from '@dword-design/functions'
 
 export default endent`
   {
+    "extends": [
+      ":semanticCommits",
+      ":semanticCommitType(fix)"
+    ],
     "lockFileMaintenance": {
       "enabled": true,
       "schedule": "at any time"
