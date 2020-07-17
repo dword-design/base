5a21e0d31e3847e3437c0726d2a10445e190c844
foo <foo@bar.de>
2020-07-17 02:36:22 +0200

    fix: remove coveralls from private relos

--- src/generated-files/github-workflow/steps/coverage.js
+++ src/generated-files/github-workflow/steps/coverage.js
@@ -1,8 +1,14 @@
-export default [
-  {
-    uses: 'coverallsapp/github-action@master',
-    with: {
-      'github-token': '${{ secrets.GITHUB_TOKEN }}',
+import loadPkg from 'load-pkg'
+
+const packageConfig = loadPkg.sync()
+
+export default packageConfig.private
+  ? []
+  : [
+    {
+      uses: 'coverallsapp/github-action@master',
+      with: {
+        'github-token': '${{ secrets.GITHUB_TOKEN }}',
+      },
     },
-  },
-]
+  ]
