bcfd023b9d994fdae6e1521ab58d9a5e63604f83
foo <foo@bar.de>
2020-08-23 15:37:51 +0200

    fix: show npm badge only if published on npm

--- src/generated-files/readme-replacements.js
+++ src/generated-files/readme-replacements.js
@@ -1,35 +1,39 @@
-import { endent } from '@dword-design/functions'
+import { endent, join } from '@dword-design/functions'
 import spdxParse from 'spdx-expression-parse'
 import spdxList from 'spdx-license-list/full'
+import packageConfig from './package-config'
+import config from '@/src/config'
 
 export default {
-  badges: packageConfig => endent`
-    [![NPM version](https://img.shields.io/npm/v/${packageConfig.name}.svg)](https://npmjs.org/package/${packageConfig.name})
-    ![Linux macOS Windows compatible](https://img.shields.io/badge/os-linux%20%7C%C2%A0macos%20%7C%C2%A0windows-blue)
-    [![Build status](https://img.shields.io/github/workflow/status/${packageConfig.repository}/build)](https://github.com/${packageConfig.repository}/actions)
-    [![Coverage status](https://img.shields.io/coveralls/${packageConfig.repository})](https://coveralls.io/github/${packageConfig.repository})
-    [![Dependency status](https://img.shields.io/david/${packageConfig.repository})](https://david-dm.org/${packageConfig.repository})
-    ![Renovate enabled](https://img.shields.io/badge/renovate-enabled-brightgreen)
+  badges: () => [
+    ...config.npmPublish ? [`[![NPM version](https://img.shields.io/npm/v/${packageConfig.name}.svg)](https://npmjs.org/package/${packageConfig.name})`] : [],
+    endent`
+      ![Linux macOS Windows compatible](https://img.shields.io/badge/os-linux%20%7C%C2%A0macos%20%7C%C2%A0windows-blue)
+      [![Build status](https://img.shields.io/github/workflow/status/${packageConfig.repository}/build)](https://github.com/${packageConfig.repository}/actions)
+      [![Coverage status](https://img.shields.io/coveralls/${packageConfig.repository})](https://coveralls.io/github/${packageConfig.repository})
+      [![Dependency status](https://img.shields.io/david/${packageConfig.repository})](https://david-dm.org/${packageConfig.repository})
+      ![Renovate enabled](https://img.shields.io/badge/renovate-enabled-brightgreen)
 
-    <a href="https://www.buymeacoffee.com/dword">
-      <img
-        src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-2.svg"
-        alt="Buy Me a Coffee"
-        height="32"
-      >
-    </a><a href="https://gitpod.io/#https://github.com/${packageConfig.repository}">
-      <img src="https://gitpod.io/button/open-in-gitpod.svg" alt="Open in Gitpod">
-    </a>
-    <a href="https://paypal.me/SebastianLandwehr">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg"
-        alt="PayPal"
-        height="30"
-      >
-    </a>
-  `,
-  description: packageConfig => packageConfig.description,
-  install: packageConfig => endent`
+      <a href="https://www.buymeacoffee.com/dword">
+        <img
+          src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-2.svg"
+          alt="Buy Me a Coffee"
+          height="32"
+        >
+      </a><a href="https://gitpod.io/#https://github.com/${packageConfig.repository}">
+        <img src="https://gitpod.io/button/open-in-gitpod.svg" alt="Open in Gitpod">
+      </a>
+      <a href="https://paypal.me/SebastianLandwehr">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg"
+          alt="PayPal"
+          height="30"
+        >
+      </a>
+    `,
+  ] |> join('\n'),
+  description: () => packageConfig.description,
+  install: () => endent`
     ## Install
 
     \`\`\`bash
@@ -40,7 +44,7 @@ export default {
     $ yarn add ${packageConfig.name}
     \`\`\`
   `,
-  license: packageConfig => {
+  license: () => {
     if (packageConfig.license) {
       const parsed = spdxParse(packageConfig.license)
       const license = spdxList[parsed.license]
@@ -58,5 +62,5 @@ export default {
     }
     return ''
   },
-  title: packageConfig => `# ${packageConfig.name}`,
+  title: () => `# ${packageConfig.name}`,
 }
