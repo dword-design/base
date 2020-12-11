cd76bd6a8d2b1a7f606171904595ac9ebb1164b8
dword <sebastianlandw@gmail.com>
2020-12-11 13:29:48 +0000

    fix: add :latest to gitpod dockerfile to enforce new node versions

--- src/generated-files/gitpod-dockerfile.js
+++ src/generated-files/gitpod-dockerfile.js
@@ -1,7 +1,8 @@
 import { endent } from '@dword-design/functions'
 
 export default endent`
-  FROM gitpod/workspace-full
+  # Need to add :latest, otherwise old versions (e.g. of node) are installed
+  FROM gitpod/workspace-full:latest
 
   RUN sudo apt-get update && sudo apt-get install -y libgtk-3-0 libx11-xcb1 libnss3 libxss1 libasound2 libgbm1
 `
