aec6b3d24b7485d79f7ee0fca0856728413abe34
Sebastian Landwehr <info@dword-design.de>
2021-02-15 02:54:27 +0100

    chore: add commitlint test

---
+++ src/index.spec.js
@@ -0,0 +1,15 @@
+import execa from 'execa'
+import withLocalTmpDir from 'with-local-tmp-dir'
+
+export default {
+  'commit message without type': () => withLocalTmpDir(async () => {
+    let output
+    try {
+      await execa.command('git commit --allow-empty -m foo', { all: true })
+    } catch (error) {
+      output = error.all
+    }
+    expect(output).toMatch('subject may not be empty [subject-empty]')
+    expect(output).toMatch('type may not be empty [type-empty]')
+  })
+}
