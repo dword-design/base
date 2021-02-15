443b5018b612a1eaca0ca9ad6751b79c48c31731
Sebastian Landwehr <info@dword-design.de>
2021-02-15 03:20:12 +0100

    chore: add commitlint test (#139)

--- src/generated-files/husky.js
+++ src/generated-files/husky.js
@@ -2,6 +2,7 @@ import commitlintPackageConfig from '@commitlint/cli/package.json'
 import { first, keys } from '@dword-design/functions'
 
 export default {
+  skipCI: false,
   hooks: {
     'commit-msg': `${
       commitlintPackageConfig.bin |> keys |> first
