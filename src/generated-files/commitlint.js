b9cc5c15daa8932eeb3bd723c9012bbc5575148f
Sebastian Landwehr <info@dword-design.de>
2021-02-16 19:50:56 +0100

    fix: remove commitlint max line length rule (#145)

--- src/generated-files/commitlint.js
+++ src/generated-files/commitlint.js
@@ -2,4 +2,7 @@ import packageName from 'depcheck-package-name'
 
 export default {
   extends: [packageName`@commitlint/config-conventional`],
+  rules: {
+    'footer-max-line-length': [0],
+  },
 }
