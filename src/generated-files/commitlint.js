fd0717b969f133feb0a19e78505d144cd214ff61
Sebastian Landwehr <info@dword-design.de>
2021-02-17 00:29:58 +0100

    fix: commitlint body max length

--- src/generated-files/commitlint.js
+++ src/generated-files/commitlint.js
@@ -3,6 +3,7 @@ import packageName from 'depcheck-package-name'
 export default {
   extends: [packageName`@commitlint/config-conventional`],
   rules: {
+    'body-max-line-length': [0],
     'footer-max-line-length': [0],
   },
 }
