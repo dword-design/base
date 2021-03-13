f1d0f65e0f0477cc2bf522bcda63900621f1498d
Sebastian Landwehr <info@dword-design.de>
2021-03-13 13:09:15 +0100

    fix: add donation cta

--- src/generated-files/readme-replacements.js
+++ src/generated-files/readme-replacements.js
@@ -1,4 +1,4 @@
-import { endent, join } from '@dword-design/functions'
+import { endent, join, compact } from '@dword-design/functions'
 import spdxParse from 'spdx-expression-parse'
 import spdxList from 'spdx-license-list/full'
 
@@ -52,23 +52,56 @@ export default {
     ] |> join('\n'),
   description: () => packageConfig.description,
   install: () => config.readmeInstallString,
-  license: () => {
-    if (packageConfig.license) {
-      const parsed = spdxParse(packageConfig.license)
-      const license = spdxList[parsed.license]
-      return endent`
-        ## License
+  license: () => [
+    endent`
+      ## ❤️ Support Me
 
-        Unless stated otherwise all works are:
+      Hey, I am Sebastian Landwehr and I love building develop web apps and open source packages. If you want to support me so that I can keep packages up to date and build more helpful tools, you can donate here:
 
-        Copyright &copy; ${packageConfig.author}
+      <a href="https://www.buymeacoffee.com/dword">
+        <img
+          src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-2.svg"
+          alt="Buy Me a Coffee"
+          height="32"
+        >
+      </a> - If you want to send me a one time donation. The coffee there is usually pretty good 😊.<br/>
 
-        and licensed under:
+      <a href="https://paypal.me/SebastianLandwehr">
+        <img
+          src="https://dword-design.de/images/paypal.svg"
+          alt="PayPal"
+          height="32"
+        >
+      </a> - Also for one time donations if you like PayPal.<br/>
 
-        [${license.name}](${license.url})
-      `
-    }
-    return ''
-  },
+      <a href="https://www.patreon.com/dworddesign">
+        <img
+          src="https://dword-design.de/images/patreon.svg"
+          alt="Patreon"
+          height="32"
+        >
+      </a> - Here you can support me regularly, which is great so I can steadily work on projects.<br/>
+    `,
+    (() => {
+      if (packageConfig.license) {
+        const parsed = spdxParse(packageConfig.license)
+        const license = spdxList[parsed.license]
+        return endent`
+          ## License
+      
+          Unless stated otherwise all works are:
+      
+          Copyright &copy; ${packageConfig.author}
+      
+          and licensed under:
+      
+          [${license.name}](${license.url})
+        `
+      }
+      return ''
+    })()
+  ]
+    |> compact
+    |> join('\n\n'),
   title: () => `# ${packageConfig.name}`,
 }
