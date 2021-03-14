b6d1b633189225254c14e7bfbcf8017d763d3c2e
Sebastian Landwehr <info@dword-design.de>
2021-03-14 19:29:54 +0100

    feat: add contributing section

--- README.md
+++ README.md
@@ -62,6 +62,10 @@ $ yarn add @dword-design/base
 <!-- /INSTALL -->
 
 <!-- LICENSE/ -->
+## Contributing
+
+Are you missing something or want to contribute? Feel free to file an [issue](https://github.com/dword-design/base/issues) or [pull request](https://github.com/dword-design/base/pulls)! ⚙️
+
 ## Support Me
 
 Hey, I am Sebastian Landwehr, a freelance web developer, and I love developing web apps and open source packages. If you want to support me so that I can keep packages up to date and build more helpful tools, you can donate here:
--- src/generated-files/readme-replacements.js
+++ src/generated-files/readme-replacements.js
@@ -95,6 +95,10 @@ export default {
   license: () =>
     [
       endent`
+      ## Contributing
+
+      Are you missing something or want to contribute? Feel free to file an [issue](https://github.com/${packageConfig.repository}/issues) or [pull request](https://github.com/${packageConfig.repository}/pulls)! ⚙️
+
       ## Support Me
 
       Hey, I am Sebastian Landwehr, a freelance web developer, and I love developing web apps and open source packages. If you want to support me so that I can keep packages up to date and build more helpful tools, you can donate here:
