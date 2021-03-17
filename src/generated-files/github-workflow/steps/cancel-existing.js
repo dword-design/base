b97bf5c9963f27c27992e07833655d10465ba999
dword <info@dword-design.de>
2021-03-17 12:27:47 +0000

    fix: upgrade rokroskar/workflow-run-cleanup-action

--- src/generated-files/github-workflow/steps/cancel-existing.js
+++ src/generated-files/github-workflow/steps/cancel-existing.js
@@ -1,6 +1,6 @@
 export default [
   {
     env: { GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}' },
-    uses: 'rokroskar/workflow-run-cleanup-action@v0.3.0',
+    uses: 'rokroskar/workflow-run-cleanup-action@v0.3.2',
   },
 ]
