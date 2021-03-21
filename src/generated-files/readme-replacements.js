0be2dccea29e72f1cd6e4c7d58c0e1f4b42067aa
dword <info@dword-design.de>
2021-03-21 09:37:35 +0000

    fix: adjust wording in readme

--- src/generated-files/readme-replacements.js
+++ src/generated-files/readme-replacements.js
@@ -95,11 +95,11 @@ export default {
   license: () =>
     [
       endent`
-      ## Contributing
+      ## Contribute
 
       Are you missing something or want to contribute? Feel free to file an [issue](https://github.com/${packageConfig.repository}/issues) or [pull request](https://github.com/${packageConfig.repository}/pulls)! ⚙️
 
-      ## Support Me
+      ## Support
 
       Hey, I am Sebastian Landwehr, a freelance web developer, and I love developing web apps and open source packages. If you want to support me so that I can keep packages up to date and build more helpful tools, you can donate here:
 
