d745884865508a9f123c75b777b92fb936ae1d39
dword <info@dword-design.de>
2021-03-25 13:34:16 +0000

    fix: ignore .nuxt folder in @babel/register

--- src/commands/test/pretest.js
+++ src/commands/test/pretest.js
@@ -8,5 +8,8 @@ expect.extend({ toMatchSnapshot })
 expect.extend({ toMatchImageSnapshot })
 // babel by default ignores everything outside the cwd and/or package.json folder. "ignore" resets this restriction
 // https://github.com/babel/babel/issues/8321
-babelRegister({ ...babelConfig, ignore: [/node_modules/] })
+babelRegister({
+  ...babelConfig,
+  ignore: [/(\/|\\)node_modules(\/|\\)/, /(\/|\\)\.nuxt(\/|\\)/],
+})
 global.expect = expect
