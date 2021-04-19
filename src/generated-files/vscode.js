8465ee645660abc79f1a60447e55b51f22def43b
Sebastian Landwehr <info@dword-design.de>
2021-04-20 01:27:13 +0200

    fix: keep unedited files open in gitpod (#317)
    * fix: keep unedited files open in gitpod
    
    * chore: update config files
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- src/generated-files/vscode.js
+++ src/generated-files/vscode.js
@@ -3,6 +3,8 @@ import { fromPairs, map } from '@dword-design/functions'
 import editorIgnoreConfig from './editor-ignore'
 
 export default {
+  'editor.tabSize': 2,
   'files.exclude':
     editorIgnoreConfig |> map(config => [config, true]) |> fromPairs,
+  'workbench.editor.enablePreview': false,
 }
