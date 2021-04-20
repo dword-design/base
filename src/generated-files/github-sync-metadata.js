0749b21b95b8ca81b76df444d359b9f65551c150
dword <info@dword-design.de>
2021-04-20 12:51:52 +0000

    fix: foo

--- src/generated-files/github-sync-metadata.js
+++ src/generated-files/github-sync-metadata.js
@@ -10,7 +10,7 @@ export default {
           uses: 'dword-design/action-sync-node-meta@fork',
           with: {
             approve: false,
-            ...!config.syncKeywords && { syncKeywords: false },
+            ...(!config.syncKeywords && { syncKeywords: false }),
             commitMessagePrefix: 'fix:',
             githubToken: '${{ secrets.GITHUB_TOKEN }}',
           },
--- src/generated-files/github-sync-metadata.spec.js
+++ src/generated-files/github-sync-metadata.spec.js
@@ -1,17 +1,17 @@
 import proxyquire from '@dword-design/proxyquire'
 
 export default {
-  valid() {
-    const self = proxyquire('./github-sync-metadata', {
-      '../config': {},
-    })
-    expect(self).toMatchSnapshot(this)
-  },
-  'do not sync keywords'() {
+  'do not sync keywords': function () {
     const self = proxyquire('./github-sync-metadata', {
       '../config': {
         syncMetadata: false,
       },
+    })
+    expect(self).toMatchSnapshot(this)
+  },
+  valid() {
+    const self = proxyquire('./github-sync-metadata', {
+      '../config': {},
     })
     expect(self).toMatchSnapshot(this)
   },
