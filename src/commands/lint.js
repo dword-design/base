e1bf109237b5d12235e25285c1e60d342ce59534
Sebastian Landwehr <info@dword-design.de>
2021-04-27 16:10:22 +0200

    fix: add eslint (#333)

--- src/commands/lint.js
+++ src/commands/lint.js
@@ -1,3 +1,29 @@
+import execa from 'execa'
+
 import config from '@/src/config'
 
-export default () => config.lint()
+export default async options => {
+  options = {
+    resolvePluginsRelativeTo: require.resolve('@dword-design/eslint-config'),
+    ...options,
+  }
+  try {
+    await execa(
+      'eslint',
+      [
+        '--fix',
+        '--ext',
+        '.js,.json,.vue',
+        '--ignore-path',
+        '.gitignore',
+        '--resolve-plugins-relative-to',
+        options.resolvePluginsRelativeTo,
+        '.',
+      ],
+      { all: true }
+    )
+  } catch (error) {
+    throw new Error(error.all)
+  }
+  await config.lint()
+}
