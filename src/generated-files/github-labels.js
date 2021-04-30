0a5783f040a3a80a4007b795b25553af7456c7c5
Sebastian Landwehr <info@dword-design.de>
2021-04-30 16:58:05 +0200

    fix: update labels (#339)
    Co-authored-by: GitHub Actions <actions@github.com>

--- src/generated-files/github-labels.js
+++ src/generated-files/github-labels.js
@@ -7,10 +7,9 @@ import {
 } from '@dword-design/functions'
 
 export default {
-  BFD4F2: ['blocking', 'breaking'],
+  BFD4F2: ['blocking', 'breaking', 'important'],
   C2E0C6: ['active', 'blocked', 'maintenance', 'waiting-for'],
   EDEDED: ['released', 'semantic-release'],
-  F9D0C4: ['important'],
 }
   |> mapValues((names, color) => names |> map(name => ({ color, name })))
   |> values
