0932b4b6e89f979ae0a7a69a341e6b7d84c747b4
dword <info@sebastianlandwehr.com>
2021-06-27 08:28:28 +0000

    fix: foo

--- src/generated-files/gitpod.js
+++ src/generated-files/gitpod.js
@@ -8,8 +8,8 @@ export default {
   ],
   vscode: {
     extensions: [
-      'karlito40.fix-irregular-whitespace@0.0.3:8jjyZYuYF6yW6nwsAiulrg==',
-      'https://dl.dropboxusercontent.com/s/efgfn4jj6ju394a/adrianwilczynski.toggle-hidden-1.0.2.vsix',
+      'https://sebastianlandwehr.com/vscode-extensions/karlito40.fix-irregular-whitespace-0.0.3.vsix',
+      'https://sebastianlandwehr.com/vscode-extensions/adrianwilczynski.toggle-hidden-1.0.2.vsix',
       'octref.vetur@0.33.1',
     ],
   },
