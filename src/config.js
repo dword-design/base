ad2dcd51f1c02751540d1cda903e30b089742296
Sebastian Landwehr <info@sebastianlandwehr.com>
2021-06-29 13:55:10 +0200

    feat: use cosmiconfig (#410)

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-import { endent, identity } from '@dword-design/functions'
+import { endent, identity, mergeAll } from '@dword-design/functions'
 import depcheck from 'depcheck'
 import depcheckDetectorExeca from 'depcheck-detector-execa'
 import depcheckDetectorPackageName from 'depcheck-detector-package-name'
@@ -6,58 +6,53 @@ import depcheckParserBabel from 'depcheck-parser-babel'
 import importCwd from 'import-cwd'
 
 import depcheckSpecialBaseConfig from './depcheck-special-base-config'
-import packageBaseConfig from './package-base-config'
 import packageConfig from './package-config'
+import rawConfig from './raw-config'
 
-const baseConfig = importCwd(packageBaseConfig.name)
+const inheritedConfig = importCwd(rawConfig.name)
 
-export default {
-  allowedMatches: [],
-  commands: {},
-  coverageFileExtensions: [],
-  deployAssets: [],
-  deployEnv: {},
-  deployPlugins: [],
-  editorIgnore: [],
-  gitignore: [],
-  lint: identity,
-  nodeVersion: 14,
-  preDeploySteps: [],
-  prepare: identity,
-  readmeInstallString: endent`
-    ## Install
-
-    \`\`\`bash
-    # npm
-    $ npm install ${packageBaseConfig.global ? '-g ' : ''}${packageConfig.name}
-
-    # Yarn
-    $ yarn ${packageBaseConfig.global ? 'global ' : ''}add ${packageConfig.name}
-    \`\`\`
-  `,
-  seeAlso: [],
-  ...baseConfig,
-  ...packageBaseConfig,
-  depcheckConfig: {
-    ignorePath: '.gitignore',
-    ...baseConfig.depcheckConfig,
-    ...packageBaseConfig.depcheckConfig,
-    detectors: [
-      depcheck.detector.importDeclaration,
-      depcheck.detector.requireCallExpression,
-      depcheck.detector.requireResolveCallExpression,
-      depcheckDetectorExeca,
-      depcheckDetectorPackageName,
-      ...(baseConfig.depcheckConfig?.detectors || []),
-    ],
-    parsers: {
-      '**/*.js': depcheckParserBabel,
-      ...baseConfig.depcheckConfig?.parsers,
+export default mergeAll([
+  {
+    allowedMatches: [],
+    commands: {},
+    coverageFileExtensions: [],
+    depcheckConfig: {
+      detectors: [
+        depcheck.detector.importDeclaration,
+        depcheck.detector.requireCallExpression,
+        depcheck.detector.requireResolveCallExpression,
+        depcheckDetectorExeca,
+        depcheckDetectorPackageName,
+      ],
+      ignorePath: '.gitignore',
+      parsers: {
+        '**/*.js': depcheckParserBabel,
+      },
+      specials: [depcheckSpecialBaseConfig, depcheck.special.bin],
     },
-    specials: [
-      depcheckSpecialBaseConfig,
-      depcheck.special.bin,
-      ...(baseConfig.depcheckConfig?.specials || []),
-    ],
+    deployAssets: [],
+    deployEnv: {},
+    deployPlugins: [],
+    editorIgnore: [],
+    gitignore: [],
+    lint: identity,
+    nodeVersion: 14,
+    preDeploySteps: [],
+    prepare: identity,
+    readmeInstallString: endent`
+      ## Install
+
+      \`\`\`bash
+      # npm
+      $ npm install ${rawConfig.global ? '-g ' : ''}${packageConfig.name}
+
+      # Yarn
+      $ yarn ${rawConfig.global ? 'global ' : ''}add ${packageConfig.name}
+      \`\`\`
+    `,
+    seeAlso: [],
+    syncKeywords: true,
   },
-}
+  inheritedConfig,
+  rawConfig,
+])
