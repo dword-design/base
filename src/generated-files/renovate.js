fb8f7dcf7644c6af3a0c6bd74b637c5c254a8e66
dword <info@sebastianlandwehr.com>
2021-07-11 18:46:55 +0000

    fix: fix

--- src/generated-files/renovate.js
+++ src/generated-files/renovate.js
@@ -5,13 +5,21 @@ export default {
   ...(packageConfig.name !== '@dword-design/base' && {
     ignorePaths: ['.github/workflows/build.yml'],
   }),
-  extends: [':semanticCommits', ':semanticPrefixFix', ':automergeMinor'],
+  extends: [':semanticCommits', ':semanticPrefixFix'],
   labels: ['maintenance'],
   lockFileMaintenance: {
+    automerge: true,
     enabled: true,
     ...(baseConfig.isLockFileFixCommitType
       ? {}
       : { semanticCommitType: 'chore' }),
   },
+  packageRules: [
+    {
+      automerge: true,
+      matchCurrentVersion: '>=1.0.0',
+      matchUpdateTypes: ['minor', 'patch'],
+    },
+  ],
   semanticCommitScope: null,
 }
