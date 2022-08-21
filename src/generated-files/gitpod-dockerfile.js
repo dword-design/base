07305ef0c168584b0d4419783d517dfad20f7fb6
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-08-21 16:29:08 +0200

    fix: set gitpod node version (#540)

--- src/generated-files/gitpod-dockerfile.js
+++ src/generated-files/gitpod-dockerfile.js
@@ -1,5 +1,7 @@
 import { endent } from '@dword-design/functions'
 
+import config from '@/src/config'
+
 export default endent`
   # Need to add :latest, otherwise old versions (e.g. of node) are installed
   FROM gitpod/workspace-full:latest
@@ -7,9 +9,19 @@ export default endent`
   RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
   RUN sudo apt-get install git-lfs
   RUN git lfs install
+
+  # https://www.gitpod.io/docs/languages/javascript
+  RUN bash -c 'VERSION="${config.nodeVersion}" \
+    && source $HOME/.nvm/nvm.sh && nvm install $VERSION \
+    && nvm use $VERSION && nvm alias default $VERSION'
+  RUN echo "nvm use default &>/dev/null" >> ~/.bashrc.d/51-nvm-fix
+
   RUN echo "\\nexport PATH=$(yarn global bin):\\$PATH" >> /home/gitpod/.bashrc
+
   RUN yarn global add gitpod-env-per-project @babel/node @babel/core
+
   RUN sudo apt-get install -y graphviz
+  
   RUN brew install gh
 
   # Puppeteer dependencies
