fc856dc991ec8e3305c3289448844b386c43e97a
Sebastian Landwehr <sebastian.landwehr@honestly.com>
2022-11-24 12:20:08 +0100

    fix: foo

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,6 +1,2 @@
 # Need to add :latest, otherwise old versions (e.g. of node) are installed
 FROM gitpod/workspace-full:2022-11-15-17-00-18
-
-RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-RUN sudo apt-get install git-lfs
-RUN git lfs install
--- src/get-generated-files/get-gitpod-dockerfile.js
+++ src/get-generated-files/get-gitpod-dockerfile.js
@@ -4,10 +4,5 @@ export default function () {
   return endent`
     # Need to add :latest, otherwise old versions (e.g. of node) are installed
     FROM gitpod/workspace-full:2022-11-15-17-00-18
-    
-    RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-    RUN sudo apt-get install git-lfs
-    RUN git lfs install
-    
   `
 }
