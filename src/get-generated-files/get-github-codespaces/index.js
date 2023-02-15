eb1bcd0de88d07fef8cc2eb5bdbcd918cff8f5ef
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-02-15 12:26:41 +0100

    fix: foo

--- src/get-generated-files/get-github-codespaces/index.js
+++ src/get-generated-files/get-github-codespaces/index.js
@@ -2,6 +2,6 @@ export default function () {
   return {
     name: 'Node.js',
     image: 'mcr.microsoft.com/devcontainers/universal:2',
-    onCreateCommand: `nvm use ${this.config.nodeVersion} && yarn --frozen-lockfile`,
+    onCreateCommand: 'echo foo',
   }
 }
