00eb903c71eb8f4b7c64ac1e7df3c9d5101e2482
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-05-06 16:43:14 +0000

    fix: upgrade docker image

--- src/get-generated-files/get-github-codespaces/index.js
+++ src/get-generated-files/get-github-codespaces/index.js
@@ -3,7 +3,7 @@ export default function () {
     features: {
       'ghcr.io/devcontainers/features/docker-in-docker:2': {},
     },
-    image: `mcr.microsoft.com/devcontainers/javascript-node:0-${this.config.nodeVersion}`,
+    image: `mcr.microsoft.com/devcontainers/javascript-node:1-${this.config.nodeVersion}`,
     updateContentCommand: 'yarn --frozen-lockfile',
   }
 }
