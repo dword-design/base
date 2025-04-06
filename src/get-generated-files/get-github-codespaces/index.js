dcd39e5a8651f5765572fdedee47e81394c892e9
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-04-06 15:15:02 +0000

    fix: remove docker-in-docker extra feature

--- src/get-generated-files/get-github-codespaces/index.js
+++ src/get-generated-files/get-github-codespaces/index.js
@@ -1,8 +1,7 @@
 export default function () {
   return {
-    features: { 'ghcr.io/devcontainers/features/docker-in-docker:2': {} },
     image: `mcr.microsoft.com/devcontainers/javascript-node:1-${this.config.nodeVersion}`,
-    updateContentCommand:
+    postCreateCommand:
       'COREPACK_INTEGRITY_KEYS=0 pnpm install --frozen-lockfile',
   };
 }
