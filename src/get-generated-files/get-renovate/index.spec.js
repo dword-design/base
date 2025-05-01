09e044c737d53717d7ef9a001fd66429dd42b21f
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-01 10:24:36 +0000

    chore: foo

--- src/get-generated-files/get-renovate/index.spec.js
+++ src/get-generated-files/get-renovate/index.spec.js
@@ -38,6 +38,31 @@ export default tester(
 
       expect(output).toMatch(/renovate\/actions-checkout-\d+\.x/);
     },
+    'lock file': async () => {
+      await execaCommand('git init');
+      await execaCommand('git config user.email "foo@bar.de"');
+      await execaCommand('git config user.name "foo"');
+
+      await fs.outputFile(
+        'package.json',
+        JSON.stringify({ dependencies: { globby: '^13.0.0' } }),
+      );
+
+      await execaCommand('pnpm install');
+      const base = new Base();
+      await base.prepare();
+      await execaCommand('git add .');
+      await execa('git', ['commit', '-m', 'feat: init']);
+
+      const output = (
+        await execaCommand('renovate --platform=local --dry-run', {
+          env: { CODESPACES: false, LOG_LEVEL: 'debug' },
+        })
+      ).stdout;
+
+      await execaCommand('git branch -avv', { stdio: 'inherit' });
+      expect(output).toMatch('chore: lock file maintenance');
+    },
     'lock file fix commit type': async () => {
       await execaCommand('git init');
       await execaCommand('git config user.email "foo@bar.de"');
@@ -61,31 +86,6 @@ export default tester(
       ).stdout;
 
       expect(output).toMatch('fix: lock file maintenance');
-    },
-    'lock file': async () => {
-      await execaCommand('git init');
-      await execaCommand('git config user.email "foo@bar.de"');
-      await execaCommand('git config user.name "foo"');
-
-      await fs.outputFile(
-        'package.json',
-        JSON.stringify({ dependencies: { globby: '14.0.0' } }),
-      );
-
-      await execaCommand('npm install');
-      const base = new Base();
-      await base.prepare();
-      await execaCommand('git add .');
-      await execa('git', ['commit', '-m', 'feat: init']);
-
-      const output = (
-        await execaCommand('renovate --platform=local --dry-run', {
-          env: { CODESPACES: false, LOG_LEVEL: 'debug' },
-        })
-      ).stdout;
-
-      await execaCommand('git branch -avv', { stdio: 'inherit' });
-      expect(output).toMatch('chore: lock file maintenance');
     },
     'nodejs version': async () => {
       await execaCommand('git init');
