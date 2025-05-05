7f2ee7b738340e595a01293b98e03733e9c6ecb5
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-05 00:22:54 +0000

    fix: foo

--- src/get-generated-files/get-github-codespaces/index.js
+++ src/get-generated-files/get-github-codespaces/index.js
@@ -1,5 +1,5 @@
 export default function () {
   return {
-    postCreateCommand: `echo 'nvm install ${this.config.nodeVersion}' >> ~/.bashrc && COREPACK_INTEGRITY_KEYS=0 pnpm install --frozen-lockfile`,
+    postCreateCommand: 'COREPACK_INTEGRITY_KEYS=0 pnpm install --frozen-lockfile',
   };
 }
