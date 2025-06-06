93d1932122ebef73fee2f44947df73dd1c93d51c
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-06-06 00:17:12 +0000

    chore: foo

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   //fullyParallel: true,
   workers: 1,
+  timeout: 60_000,
   preserveOutput: 'failures-only',
   snapshotPathTemplate:
     '{snapshotDir}/{testFileDir}/{testFileName}-snapshots/{arg}{-projectName}{ext}',
