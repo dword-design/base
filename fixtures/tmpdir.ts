2d64935baaeb0d4b5719c6c629b0a6eaa6233ddc
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-05 23:00:52 +0200

    fix: remove typescript include src

--- fixtures/tmpdir.ts
+++ fixtures/tmpdir.ts
@@ -5,7 +5,7 @@ import { test as base } from '@playwright/test';
 import fs from 'fs-extra';
 import { v4 as uuid } from 'uuid';
 
-export const test = base.extend({
+export const test = base.extend<{ tmpdir: string }>({
   tmpdir: async ({}, use) => {
     const cwd = pathLib.join(createTmpDir(), uuid());
 
