19f2bc3f3ecb26565b23ec98043bc223475ef1dc
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-17 18:23:06 +0200

    fix: add env to CommandOptionsInput

--- src/commands/command-options-input.d.ts
+++ src/commands/command-options-input.d.ts
@@ -3,4 +3,5 @@ import type { StdoutStderrOptionCommon } from 'execa';
 export type CommandOptionsInput = {
   log?: boolean;
   stderr?: StdoutStderrOptionCommon<false>;
+  env?: Record<string, string>;
 };
