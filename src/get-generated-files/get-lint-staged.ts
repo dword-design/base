70785b3ec123f33ef3e345297731bc04a5aa9b43
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-08-07 06:48:08 +0200

    fix: merge lint staged (#811)

--- src/get-generated-files/get-lint-staged.ts
+++
@@ -1,8 +0,0 @@
-import type { Configuration as LintStagedConfig } from 'lint-staged';
-
-export default function (): LintStagedConfig {
-  return {
-    '*.{json,ts,vue}': 'eslint --fix --config eslint.lint-staged.config.ts',
-    ...this.config.lintStaged,
-  };
-}
