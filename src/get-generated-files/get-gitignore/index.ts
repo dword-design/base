7b0f7c7d8e43b2d56746d804fa66c70a70500fab
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-02 00:43:32 +0100

    fix: ignore pnpm-store

--- src/get-generated-files/get-gitignore/index.ts
+++ src/get-generated-files/get-gitignore/index.ts
@@ -3,13 +3,14 @@ import { identity, sortBy } from 'lodash-es';
 export default function () {
   return sortBy(
     [
-      '/codecov',
-      '/codecov.SHA256SUM',
-      '/codecov.SHA256SUM.sig',
       '.DS_Store',
+      '/.pnpm-store', // https://github.com/orgs/pnpm/discussions/6936#discussioncomment-6746063
       '/.env.json',
       '/.test.env.json',
       '/.nyc_output',
+      '/codecov',
+      '/codecov.SHA256SUM',
+      '/codecov.SHA256SUM.sig',
       '/coverage',
       '/node_modules',
       '/test-results',
