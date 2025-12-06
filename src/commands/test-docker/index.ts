49030247a97cd12830dc341992de517e902e92f8
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-06 23:14:21 +0100

    fix: drop chown docker logic

--- src/commands/test-docker/index.ts
+++ src/commands/test-docker/index.ts
@@ -1,5 +1,3 @@
-import { userInfo as getUserInfo } from 'node:os';
-
 import { constantCase } from 'change-case';
 import { execa } from 'execa';
 import { findUpSync } from 'find-up';
@@ -21,56 +19,34 @@ export default async function (options) {
     ...(envSchemaPath ? await fs.readJson(envSchemaPath) : {}),
   }).map(name => constantCase(name));
 
-  const userInfo = getUserInfo();
-
-  try {
-    return await execa(
-      'docker',
+  return await execa(
+    'docker',
+    [
+      'run',
+      '--rm',
+      ...envVariableNames
+        .filter(name => process.env[name] !== undefined)
+        .flatMap(name => ['--env', `${name}=${process.env[name]}`]),
+      '-v',
+      `${this.cwd}:/app`,
+      '-v',
+      `${volumeName}:/app/node_modules`,
+      'dworddesign/testing:latest',
+      'bash',
+      '-c',
       [
-        'run',
-        '--rm',
-        ...envVariableNames
-          .filter(name => process.env[name] !== undefined)
-          .flatMap(name => ['--env', `${name}=${process.env[name]}`]),
-        '-v',
-        `${this.cwd}:/app`,
-        '-v',
-        `${volumeName}:/app/node_modules`,
-        'dworddesign/testing:latest',
-        'bash',
-        '-c',
-        [
-          'pnpm install --frozen-lockfile',
-          '&&',
-          'pnpm test:raw',
-          ...(options.updateSnapshots ? [' --update-snapshots'] : []),
-          ...options.patterns.map(pattern => `"${pattern}"`),
-          ...(options.grep ? [`-g "${options.grep}"`] : []),
-        ].join(' '),
-      ],
-      {
-        cwd: this.cwd,
-        ...(options.log && { stdout: 'inherit' }),
-        stderr: options.stderr,
-      },
-    );
-  } finally {
-    await execa(
-      'docker',
-      [
-        'run',
-        '--rm',
-        '--tty',
-        '-v',
-        `${this.cwd}:/app`,
-        '-v',
-        `${volumeName}:/app/node_modules`,
-        'dworddesign/testing:latest',
-        'bash',
-        '-c',
-        `chown -R ${userInfo.uid}:${userInfo.gid} /app`,
-      ],
-      { cwd: this.cwd },
-    );
-  }
+        'pnpm install --frozen-lockfile',
+        '&&',
+        'pnpm test:raw',
+        ...(options.updateSnapshots ? [' --update-snapshots'] : []),
+        ...options.patterns.map(pattern => `"${pattern}"`),
+        ...(options.grep ? [`-g "${options.grep}"`] : []),
+      ].join(' '),
+    ],
+    {
+      cwd: this.cwd,
+      ...(options.log && { stdout: 'inherit' }),
+      stderr: options.stderr,
+    },
+  );
 }
