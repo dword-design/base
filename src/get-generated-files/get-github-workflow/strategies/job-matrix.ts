efb59bfae33306320b66a879d7f0e0a02762dcf5
Sebastian Landwehr <info@sebastianlandwehr.com>
2026-01-07 20:57:00 +0100

    fix: env variables for lint before release and drop GH_TOKEN (#833)

--- src/get-generated-files/get-github-workflow/strategies/job-matrix.ts
+++ src/get-generated-files/get-github-workflow/strategies/job-matrix.ts
@@ -1,3 +1,9 @@
+import pathLib from 'node:path';
+
+import { constantCase } from 'change-case';
+import { findUpStop, findUpSync } from 'find-up';
+import fs from 'fs-extra';
+import parsePackagejsonName from 'parse-packagejson-name';
 import gitHubAction from 'tagged-template-noop';
 
 import coverageSteps from '@/src/get-generated-files/get-github-workflow/steps/coverage';
@@ -5,6 +11,32 @@ import getReleaseSteps from '@/src/get-generated-files/get-github-workflow/steps
 import getTestSteps from '@/src/get-generated-files/get-github-workflow/steps/get-test';
 
 export default function () {
+  const envSchemaPath = findUpSync(
+    path => {
+      if (fs.existsSync(pathLib.join(path, '.env.schema.json'))) {
+        return '.env.schema.json';
+      }
+
+      if (fs.existsSync(pathLib.join(path, 'package.json'))) {
+        return findUpStop;
+      }
+    },
+    { cwd: this.cwd },
+  );
+
+  const localEnvVariableNames = Object.keys(
+    envSchemaPath ? fs.readJsonSync(envSchemaPath) : {},
+  ).map(name => constantCase(name));
+
+  const envVariables = {
+    ...(this.config.doppler
+      ? { DOPPLER_TOKEN: '${{ secrets.DOPPLER_TOKEN }}' }
+      : Object.fromEntries(
+          localEnvVariableNames.map(name => [name, `\${{ secrets.${name} }}`]),
+        )),
+  };
+
+  const packageName = parsePackagejsonName(this.packageConfig.name).fullName;
   return {
     release: {
       needs: 'test',
@@ -25,7 +57,12 @@ export default function () {
         { run: 'git config --global user.email "actions@github.com"' },
         { run: 'git config --global user.name "GitHub Actions"' },
         { run: 'pnpm install --frozen-lockfile' },
-        { run: 'pnpm lint' },
+        {
+          ...(Object.keys(envVariables).length > 0
+            ? { env: envVariables }
+            : {}),
+          run: `${this.config.doppler ? `doppler run -p ${packageName} -c test -- ` : ''}pnpm lint`,
+        },
         ...getReleaseSteps.call(this),
       ],
     },
