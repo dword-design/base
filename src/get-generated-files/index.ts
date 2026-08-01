1232049837362bfb65d534316685009f896264b3
Sebastian Landwehr <info@sebastianlandwehr.com>
2026-08-01 15:03:35 +0200

    fix: fixes

--- src/get-generated-files/index.ts
+++ src/get-generated-files/index.ts
@@ -23,69 +23,66 @@ import npmrc from './npmrc';
 
 const resolver = createRequire(import.meta.url);
 
-export default (base: Base) => {
-  const packageConfig = base.getPackageConfig();
-  return {
-    '.commitlintrc.json': `${JSON.stringify(commitlintConfig, undefined, 2)}\n`,
-    '.cz.json': `${JSON.stringify(commitizenConfig, undefined, 2)}\n`,
-    '.devcontainer/devcontainer.json': `${JSON.stringify(
-      githubCodespacesConfig,
-      undefined,
-      2,
-    )}\n`,
-    '.editorconfig': editorconfigConfig,
-    '.gitattributes': gitattributesConfig,
-    '.github/DEPRECATED_DEPENDENCIES_ISSUE_TEMPLATE.md':
-      deprecatedDependenciesIssueTemplate,
-    '.github/FUNDING.yml': yamlStringify(githubFunding),
-    '.github/labels.yml': yamlStringify(
-      sortKeys(githubLabelsConfig, { deep: true }),
-    ),
-    '.github/workflows/build.yml': yamlStringify(
-      sortKeys(base.getGithubWorkflowConfig(), { deep: true }),
-    ),
-    '.github/workflows/deprecated-dependencies.yml': yamlStringify(
-      sortKeys(githubDeprecatedDependenciesConfig, { deep: true }),
-    ),
-    '.github/workflows/sync-labels.yml': yamlStringify(
-      sortKeys(githubSyncLabelsConfig, { deep: true }),
-    ),
-    '.github/workflows/sync-metadata.yml': yamlStringify(
-      sortKeys(base.getGithubSyncMetadataConfig(), { deep: true }),
-    ),
-    '.gitignore': base
-      .getGitignoreConfig()
-      .map(entry => `${entry}\n`)
-      .join(''),
-    '.gitpod.Dockerfile': base.getGitpodDockerfile(),
-    '.gitpod.yml': yamlStringify(base.getGitpodConfig()),
-    '.lintstagedrc.json': `${JSON.stringify(base.getLintStaged(), undefined, 2)}\n`,
-    '.npmrc': stringifyIni(npmrc),
-    '.releaserc.json': `${JSON.stringify(
-      base.getReleaseConfig(),
-      undefined,
-      2,
-    )}\n`,
-    '.renovaterc.json': `${JSON.stringify(
-      sortKeys(base.getRenovateConfig(), { deep: true }),
-      undefined,
-      2,
-    )}\n`,
-    '.vscode/settings.json': `${JSON.stringify(
-      base.getVscodeConfig(),
-      undefined,
-      2,
-    )}\n`,
-    'AGENTS.md': fs.readFileSync(resolver.resolve('./agents.md'), 'utf8'),
-    'eslint.config.ts': base.getEslintConfig(),
-    'eslint.lint-staged.config.ts': eslintLintStaged,
-    'LICENSE.md': base.getLicenseString(),
-    'package.json': `${JSON.stringify(
-      sortPackageJson(packageConfig),
-      undefined,
-      2,
-    )}\n`,
-    'README.md': base.getReadmeString(),
-    'tsconfig.json': `${JSON.stringify(sortKeys(base.getTypescriptConfig(), { deep: true }), undefined, 2)}\n`,
-  };
-};
+export default (base: Base) => ({
+  '.commitlintrc.json': `${JSON.stringify(commitlintConfig, undefined, 2)}\n`,
+  '.cz.json': `${JSON.stringify(commitizenConfig, undefined, 2)}\n`,
+  '.devcontainer/devcontainer.json': `${JSON.stringify(
+    githubCodespacesConfig,
+    undefined,
+    2,
+  )}\n`,
+  '.editorconfig': editorconfigConfig,
+  '.gitattributes': gitattributesConfig,
+  '.github/DEPRECATED_DEPENDENCIES_ISSUE_TEMPLATE.md':
+    deprecatedDependenciesIssueTemplate,
+  '.github/FUNDING.yml': yamlStringify(githubFunding),
+  '.github/labels.yml': yamlStringify(
+    sortKeys(githubLabelsConfig, { deep: true }),
+  ),
+  '.github/workflows/build.yml': yamlStringify(
+    sortKeys(base.getGithubWorkflowConfig(), { deep: true }),
+  ),
+  '.github/workflows/deprecated-dependencies.yml': yamlStringify(
+    sortKeys(githubDeprecatedDependenciesConfig, { deep: true }),
+  ),
+  '.github/workflows/sync-labels.yml': yamlStringify(
+    sortKeys(githubSyncLabelsConfig, { deep: true }),
+  ),
+  '.github/workflows/sync-metadata.yml': yamlStringify(
+    sortKeys(base.getGithubSyncMetadataConfig(), { deep: true }),
+  ),
+  '.gitignore': base
+    .getGitignoreConfig()
+    .map(entry => `${entry}\n`)
+    .join(''),
+  '.gitpod.Dockerfile': base.getGitpodDockerfile(),
+  '.gitpod.yml': yamlStringify(base.getGitpodConfig()),
+  '.lintstagedrc.json': `${JSON.stringify(base.getLintStaged(), undefined, 2)}\n`,
+  '.npmrc': stringifyIni(npmrc),
+  '.releaserc.json': `${JSON.stringify(
+    base.getReleaseConfig(),
+    undefined,
+    2,
+  )}\n`,
+  '.renovaterc.json': `${JSON.stringify(
+    sortKeys(base.getRenovateConfig(), { deep: true }),
+    undefined,
+    2,
+  )}\n`,
+  '.vscode/settings.json': `${JSON.stringify(
+    base.getVscodeConfig(),
+    undefined,
+    2,
+  )}\n`,
+  'AGENTS.md': fs.readFileSync(resolver.resolve('./agents.md'), 'utf8'),
+  'eslint.config.ts': base.getEslintConfig(),
+  'eslint.lint-staged.config.ts': eslintLintStaged,
+  'LICENSE.md': base.getLicenseString(),
+  'package.json': `${JSON.stringify(
+    sortPackageJson(base.packageConfig),
+    undefined,
+    2,
+  )}\n`,
+  'README.md': base.getReadmeString(),
+  'tsconfig.json': `${JSON.stringify(sortKeys(base.getTypescriptConfig(), { deep: true }), undefined, 2)}\n`,
+});
