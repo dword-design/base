8596483a397f4a0f36ee43b6e6dca38867670cb7
Sebastian Landwehr <info@sebastianlandwehr.com>
2026-08-01 02:15:49 +0200

    fix: foo

--- src/get-depcheck-special-base/index.ts
+++ src/get-depcheck-special-base/index.ts
@@ -1,6 +1,6 @@
 import pathLib from 'node:path';
 
-export default name => filePath =>
+export default (name?: string) => (filePath: string) =>
   name !== '@dword-design/base-config-node' &&
   pathLib.basename(filePath) === 'package.json'
     ? [name]
